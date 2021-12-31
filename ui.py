92877c83b1786e9b2216bd18238ff541aac3c1c0
Josh H (santiago) <hendershotjoshr@gmail.com>
2021-12-30 22:04:51 -0400

    tried to paste text but doesn;t work yet

--- ui.py
+++ ui.py
@@ -5,9 +5,17 @@ class SpellcheckUI:
         self.window = tk.Tk()
         text_box = tk.Text()
         text_box.pack()
+        self.post_text_box = text_box
+        tk.Button(text='paste post', command=self.paste_post).pack()
+
+    def paste_post(self):
+        self.post_text_box.insert("0", "this is a test, it should be in the text box.\nwith a new line here :)")
+
+    def mainloop(self):
         self.window.mainloop()
         
 
 if __name__ == "__main__":
     sp = SpellcheckUI()
     sp.display()
+    sp.mainloop()
