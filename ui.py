174554bfee1d71ef14aa3a08dbbfb26f5be525be
Josh H (santiago) <hendershotjoshr@gmail.com>
2022-01-01 10:54:36 -0400

    fixed text pasting

--- ui.py
+++ ui.py
@@ -9,7 +9,8 @@ class SpellcheckUI:
         tk.Button(text='paste post', command=self.paste_post).pack()
 
     def paste_post(self):
-        self.post_text_box.insert("0", "this is a test, it should be in the text box.\nwith a new line here :)")
+        self.post_text_box.delete("1.0", tk.END)
+        self.post_text_box.insert("1.0", "this is a test, it should be in the text box.\nwith a new line here :)")
 
     def mainloop(self):
         self.window.mainloop()
