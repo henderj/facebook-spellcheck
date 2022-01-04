72c7382a60ec6b6fce5b7c430c2563def991f042
Josh H (santiago) <hendershotjoshr@gmail.com>
2022-01-03 22:21:10 -0400

    added widgets

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ from json import encoder
 from typing import Dict, Any
 from facebook import GraphAPI
 import requests
-from secrets import accessToken 
+from secrets import accessToken
 from spellcheck import put_text_into_spellboy
+from ui import SpellcheckUI
 
 # santiago
 page_id = 112473910504378
+
 
 def retrive_posts() -> Dict:
     graph = GraphAPI(access_token=accessToken, version=2.12)
     posts = graph.request(f"{page_id}/posts")
     return posts
 
+
 def save_posts_to_file(posts: Dict) -> None:
     js_text = json.dumps(posts, indent=2)
     with open("posts.json", "w", encoding="utf-8") as f:
         f.write(js_text)
+
 
 def retrive_posts_from_file() -> Dict:
     with open("posts.json") as f:
@@ -28,23 +32,26 @@ def retrive_posts_from_file() -> Dict:
 
 def update_post(post_id: int, message: str) -> Any:
     payload = {
-        'message': message,
+        "message": message,
     }
     graph = GraphAPI(access_token=accessToken, version=2.12)
-    result = graph.request(f'{post_id}/', post_args=payload)
+    result = graph.request(f"{post_id}/", post_args=payload)
     return result
 
+
 def main():
-    data = retrive_posts()
+    data = retrive_posts_from_file()
     posts = data["data"]
-    first_post = posts[0]
-    first_m = first_post["message"]
-    print(first_m)
-    post_id = first_post["id"]
-    print(post_id)
+    ui = SpellcheckUI(posts)
+    ui.display()
+    # first_post = posts[0]
+    # first_m = first_post["message"]
+    # print(first_m)
+    # post_id = first_post["id"]
+    # print(post_id)
     # new_text = put_text_into_spellboy(first_m)
     # result = update_post(post_id, new_text)
-    # print(result) 
+    # print(result)
 
 
 if __name__ == "__main__":
--- ui.py
+++ ui.py
@@ -1,22 +1,55 @@
 import tkinter as tk
+from tkinter import StringVar, ttk
+from tkinter.constants import N, W, E, S
 
 class SpellcheckUI:
+    def __init__(self, posts) -> None:
+        self._posts = posts
+
     def display(self):
-        self.window = tk.Tk()
-        text_box = tk.Text()
-        text_box.pack()
-        self.post_text_box = text_box
-        tk.Button(text='paste post', command=self.paste_post).pack()
+        root = tk.Tk()
+        mainframe = ttk.Frame(root, padding="3 3 12 12")
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=0)
+
+        page_selection_text = StringVar(value="Select a page...")
+        page_selection = ttk.Combobox(mainframe,
+            textvariable=page_selection_text, 
+            values=["Santiago", "Puerto Plata", "La Vega", "Navarrete"])
+        page_selection.state(["readonly"])
+        page_selection.bind("<<ComboboxSelected>>", self.page_changed)
+        page_selection.grid(column=0, columnspan=2, row=0)
+        self.page_selection = page_selection
+
+        tk.Button(mainframe, text="load posts", command=self.paste_post)\
+            .grid(column=2, row=0)
+
+        text_box = tk.Text(mainframe, undo=True)
+        text_box.grid(column=0, columnspan=3, row=1)
+        self._post_text_box = text_box
+
+
+        tk.Button(mainframe, text="previous", command=self.paste_post)\
+            .grid(column=0, row=2)
+        tk.Button(mainframe, text="update", command=self.paste_post)\
+            .grid(column=1, row=2)            
+        tk.Button(mainframe, text="next", command=self.paste_post)\
+            .grid(column=2, row=2)
+
+        root.mainloop()
+
+    def page_changed(self, arg1):
+        self.page_selection.selection_clear()
 
     def paste_post(self):
-        self.post_text_box.delete("1.0", tk.END)
-        self.post_text_box.insert("1.0", "this is a test, it should be in the text box.\nwith a new line here :)")
+        self._post_text_box.delete("1.0", tk.END)
+        self._post_text_box.insert(
+            "1.0",
+            "this is a test, it should be in the text box.\nwith a new line here :)",
+        )
 
-    def mainloop(self):
-        self.window.mainloop()
-        
 
 if __name__ == "__main__":
-    sp = SpellcheckUI()
+    sp = SpellcheckUI([])
     sp.display()
-    sp.mainloop()
